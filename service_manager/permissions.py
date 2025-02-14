77b2563159b1f05464ffd5b364e00c2d6fd4b1e0
Abhiinav-Kumar <abhinavkumarpa01@gmail.com>
2025-02-14 22:04:14 +0530

    Fix : role access

--- service_manager/permissions.py
+++ service_manager/permissions.py
@@ -1,27 +1,27 @@
-from rest_framework.permissions import BasePermission
-from accounts.models import UserRegister
+# from rest_framework.permissions import BasePermission
+# from accounts.models import UserRegister
 
-import logging
+# import logging
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-class IsCustomer(BasePermission):
+# class IsCustomer(BasePermission):
     
-    def has_permission(self, request, view):
+#     def has_permission(self, request, view):
         
-        logger.info(f"User: {request.user}, Authenticated: {request.user.is_authenticated}")
+#         logger.info(f"User: {request.user}, Authenticated: {request.user.is_authenticated}")
 
-        # Ensure user is authenticated
-        if not request.user or not request.user.is_authenticated:
-            logger.warning("Access denied: User is not authenticated")
-            return False
+#         # Ensure user is authenticated
+#         if not request.user or not request.user.is_authenticated:
+#             logger.warning("Access denied: User is not authenticated")
+#             return False
         
-        # Ensure user has a role and it is 'customer'
-        user_role = getattr(request.user, 'role', None)
-        logger.info(f"User Role: {user_role}")
+#         # Ensure user has a role and it is 'customer'
+#         user_role = getattr(request.user, 'role', None)
+#         logger.info(f"User Role: {user_role}")
 
-        if user_role != "customer":
-            logger.warning("Access denied: User is not a customer")
-            return False
+#         if user_role != "customer":
+#             logger.warning("Access denied: User is not a customer")
+#             return False
         
-        return True
+#         return True
